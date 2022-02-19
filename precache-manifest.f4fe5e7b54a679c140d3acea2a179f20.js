e60d9c7297c195cd43f82092b86c2af0d4432e81
Shachar Ettner <shacharliraz@gmail.com>
2022-02-19 23:42:44 +0200

    Updates

---
+++ precache-manifest.f4fe5e7b54a679c140d3acea2a179f20.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4332b916d759a42d56946141413a39",
+    "url": "/loop-music/index.html"
+  },
+  {
+    "revision": "10f894ba2feac5a524b2",
+    "url": "/loop-music/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "60a040b74defcd36f104",
+    "url": "/loop-music/static/css/main.c7d9d93e.chunk.css"
+  },
+  {
+    "revision": "10f894ba2feac5a524b2",
+    "url": "/loop-music/static/js/2.644b15dd.chunk.js"
+  },
+  {
+    "revision": "9ac7d1fb223f2f9a1e9e",
+    "url": "/loop-music/static/js/3.79a04dbb.chunk.js"
+  },
+  {
+    "revision": "60a040b74defcd36f104",
+    "url": "/loop-music/static/js/main.0116d20f.chunk.js"
+  },
+  {
+    "revision": "53087e96385fb606d1eb",
+    "url": "/loop-music/static/js/runtime~main.4061923f.js"
+  },
+  {
+    "revision": "c435443f9b3efbdd8c0a57a89017b73c",
+    "url": "/loop-music/static/media/B VOC.c435443f.mp3"
+  },
+  {
+    "revision": "e3e2854f05d2d6bead5fc8d3dacd56bc",
+    "url": "/loop-music/static/media/DRUMS.e3e2854f.mp3"
+  },
+  {
+    "revision": "4db5581dd48f0099773a6e6197ab2f58",
+    "url": "/loop-music/static/media/HE HE VOC.4db5581d.mp3"
+  },
+  {
+    "revision": "d02c416417e234e3a7463bbce5646fc1",
+    "url": "/loop-music/static/media/HIGH VOC.d02c4164.mp3"
+  },
+  {
+    "revision": "11a9984be6849086e3fb99b8694e626d",
+    "url": "/loop-music/static/media/JIBRISH.11a9984b.mp3"
+  },
+  {
+    "revision": "dadfe26a217e4eed5a4194eb7706edd3",
+    "url": "/loop-music/static/media/LEAD 1.dadfe26a.mp3"
+  },
+  {
+    "revision": "b68f64b5c79974187e329e05616be978",
+    "url": "/loop-music/static/media/UUHO VOC.b68f64b5.mp3"
+  },
+  {
+    "revision": "06c14acedf5b2209306ff0ca2ee547af",
+    "url": "/loop-music/static/media/_tambourine_shake_higher.06c14ace.mp3"
+  }
+]);
